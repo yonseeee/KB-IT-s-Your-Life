73ebcf7b44f62b826d6642d283f05e5748c5bf9b
Seyeon <marks2yeon@gmail.com>
2025-03-20 17:50:26 +0900

    250320

--- 02_VUE/02_Vue/slice.js
+++
@@ -1,8 +0,0 @@
-const ar = [0, 1, 2, 3, 4, 5, 6, 7];
-const subar = ar.slice(2, 5);
-console.log('ar = ' + ar);
-console.log('subar = ' + subar);
-
-const ar2 = [0, 1, 2, 3, 4, 5, 6, 7];
-ar2.splice(2, 3);
-console.log('ar2 = ' + ar2);
